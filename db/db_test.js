4b36cc6b5af28c821116c4b2d423892f08bb68af
wdra233 <wangyawang666@gmail.com>
2021-06-14 22:22:23 -0700

    hide mongo_db_url

--- db/db_test.js
+++ db/db_test.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://ywang:wdra233@cluster0.lz5rm.mongodb.net/job-radar?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_DB_URL);
 const conn = mongoose.connection;
 conn.on('connected', () => {
     console.log('Successfully connected!');
